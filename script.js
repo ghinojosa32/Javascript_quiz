8b5935a1fff038a1e9d16c11e7312ed208cf58d9
Gustavo <gusbuss_1@outlook.com>
2021-05-24 21:22:26 -0500

    I created variables for start game function and added an event listener to start the timer and quiz

--- script.js
+++ script.js
@@ -1,15 +1,56 @@
-// creating elements
-var  Rules = document.querySelector(".Rules");
-var continue_btn = Rules.querySelector(".continue_btn");
-var start_btn = document.querySelector(".start_btn");
-var timerElement = document. querySelector("timer_sec");
-
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+var isSubmit = false;
 var timer;
 var timerCount;
 
 
+   // variable to store the HTML output
+   var output = [];
 
+
+ function startQuiz(){
+
+
+  // Functions
+  function startQuiz(){
+    isSubmit = false;
+    timerCount = 20;
+    startButton.disabled = true;
+    startTimer()
+  }
+
+    function endQuiz() {
+      window.alert = "Out of time";
+      startButton.disabled = false;
+      showResults()
+    }
+
+
+    // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+function startTimer() {
+  // Sets timer
+  timer = setInterval(function() {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount >= 0) {
+      // Tests if win condition is met
+      if (isSubmit&& timerCount > 0) {
+        // Clears interval and stops timer
+        clearInterval(timer);
+      }
+    }
+    // Tests if time has run out
+    if (timerCount === 0) {
+      // Clears interval
+      clearInterval(timer);
+      endQuiz();
+    }
+  }, 1000);
 
 }
+
+startButton.addEventListener("click", startQuiz);
+
 
 
