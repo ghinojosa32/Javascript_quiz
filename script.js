ad25cd220b154f5d2cf172112b102c7b66c05d3f
Gustavo <gusbuss_1@outlook.com>
2021-05-24 21:20:41 -0500

    I deleted what I had and started over because I did it wrong.

---
+++ script.js
@@ -0,0 +1,15 @@
+// creating elements
+var  Rules = document.querySelector(".Rules");
+var continue_btn = Rules.querySelector(".continue_btn");
+var start_btn = document.querySelector(".start_btn");
+var timerElement = document. querySelector("timer_sec");
+
+var timer;
+var timerCount;
+
+
+
+
+}
+
+
